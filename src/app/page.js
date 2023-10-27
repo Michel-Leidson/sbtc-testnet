f1306f8bdf35d1a8e304139e62fbfc311429da9a
Michel-Leidson <contact@stakepool.dev.br>
2023-10-27 12:19:38 +0000

    sbtc-testnet

---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+export const metadata = {
+  title: "StakePool-Sbtc-2-task",
+  description: "A decentralized Bitcoin lending application",
+};
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="mt-8 text-4xl text-center">Lagoon</h1>
+      <p className="mt-4 text-center">
+        Decentralized lending and borrowing with sBTC.
+      </p>
+    </>
+  );
+}
